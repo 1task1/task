627cdc31013105b69d6c6be6c2c3e64bfd3eff49
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-03-13 01:31:29 +0400

    hello

---
+++ git/git.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Version1" << "Hello World!\n";
+	cout << "Version1" << "Hello!\n";
+	cout << "Version2" << "Hello2!\n";
+	cout << "Version2" << "Hello21!\n";
+	cout << "Version3" << "Hello211!\n";
+	cout << "Version3" << "Hello213!\n";
+}

aa9f50aa688c2ef171f5cb7b5f0968c557e645f3
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-03-13 01:32:52 +0400

    hello2

--- git/git.cpp
+++ git/git.cpp
@@ -4,8 +4,13 @@ int main()
 {
 	cout << "Version1" << "Hello World!\n";
 	cout << "Version1" << "Hello!\n";
-	cout << "Version2" << "Hello2!\n";
-	cout << "Version2" << "Hello21!\n";
-	cout << "Version3" << "Hello211!\n";
-	cout << "Version3" << "Hello213!\n";
+	cout << "Version1" << "Hello2!\n";
+	cout << "Version1" << "Hello21!\n";
+	cout << "Version1" << "Hello211!\n";
+	cout << "Version1" << "Hello213!\n";
+	
+	
+	
+	cout << "Version2" << "Hello213!\n";
+	cout << "Version2" << "Hello213!\n";
 }
